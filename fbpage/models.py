738cba5988105595efaa76dd849c0be2dfa7c616
Namwoo Kim <iam@kimtree.net>
2015-05-27 16:52:45 +0900

    a

--- fbpage/models.py
+++ fbpage/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class FbPage(models.Model):
+    def __unicode__(self):
+        return self.name
+
     page_id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     icon_url = models.URLField()
