2dde753102cb640fde0466dd18372e486c30f607
Namwoo Kim <iam@kimtree.net>
2015-05-28 15:07:29 +0900

    wsgi

--- fedong/wsgi.py
+++ fedong/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-root_path = os.path.abspath(os.path.split(__file__)[0])
-sys.path.insert(0, os.path.join(root_path, 'fedong'))
-sys.path.insert(0, root_path)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", ".settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fedong.settings.prod")
 
 application = get_wsgi_application()
