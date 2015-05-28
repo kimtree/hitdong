5d55083d64c47fcf3e04c68e8fdc576cd3af36b9
Namwoo Kim <iam@kimtree.net>
2015-05-28 14:10:15 +0900

    f

--- fedong/wsgi.py
+++ fedong/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, root_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fedong.settings.prod")
 
