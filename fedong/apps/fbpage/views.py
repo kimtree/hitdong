0eadbc010a4fb9992a5e88f7a7a639a86d81009e
Namwoo Kim <iam@kimtree.net>
2015-06-09 22:08:11 +0900

    Update page crawler

--- fedong/apps/fbpage/views.py
+++ fedong/apps/fbpage/views.py
@@ -1,3 +1,4 @@
+import time
 import threading
 import Queue
 
@@ -5,6 +6,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
 from django.shortcuts import render
 from django.template import *
+from django.http import HttpResponse
 
 from fedong.apps.crawler.crawler import PageCrawler
 from fedong.apps.fbpage.models import FbPage
@@ -36,22 +38,40 @@ def crawler(request):
     pages = FbPage.objects.all()
 
     data_queue = Queue.Queue()
+    output_queue = Queue.Queue()
     for page in pages:
         data_queue.put((page.username, settings.FACEBOOK_ACCESS_TOKEN))
 
+    start_time = time.time()
+
     # Run Crawler
-    for i in range(4):
-        t = PageThread(data_queue)
+    for i in range(20):
+        t = PageThread(data_queue, output_queue)
         t.setDaemon(True)
         t.start()
 
     data_queue.join()
 
+    while not output_queue.empty():
+        p = output_queue.get()
+
+        f = FbPage.objects.filter(username=p.username)[0]
+        if f:
+            print p.page_id, p.name
+            f.page_id = p.page_id
+            f.name = p.name
+            f.profile_url = p.profile_url
+            f.likes = p.likes
+            f.save()
+
+    return HttpResponse('%s seconds' % (time.time() - start_time))
+
 
 class PageThread(threading.Thread):
-    def __init__(self, data_queue):
+    def __init__(self, data_queue, output_queue):
         threading.Thread.__init__(self)
         self.data_queue = data_queue
+        self.output_queue = output_queue
 
     def run(self):
         while True:
@@ -60,13 +80,6 @@ class PageThread(threading.Thread):
             p = PageCrawler(username, access_token)
             p.run()
 
-            f = FbPage.objects.filter(username=username)[0]
-            if f:
-                print p.page_id, p.name
-                f.page_id = p.page_id
-                f.name = p.name
-                f.profile_url = p.profile_url
-                f.likes = p.likes
-                f.save()
+            self.output_queue.put(p)
 
             self.data_queue.task_done()
