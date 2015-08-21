f3f24a10a1c6e57fe51078946d1c02b5d9df24f4
Namwoo Kim <iam@kimtree.net>
2015-08-22 00:33:36 +0900

    Modify video order

--- hitdong/apps/video/views.py
+++ hitdong/apps/video/views.py
@@ -13,8 +13,9 @@ from hitdong.apps.video.tasks import crawl_videos
 def main(request):
     video_list = Video.objects.select_related()
     video_list = video_list.filter(is_open=True)
-    video_list = video_list.extra(select={'date': 'date(created_at)'})
-    video_list = video_list.order_by('-date', '-metric', '-created_at')
+    # video_list = video_list.extra(select={'date': 'date(created_at)'})
+    # video_list = video_list.order_by('-date', '-metric', '-created_at')
+    video_list = video_list.order_by('-created_at')
 
     paginator = Paginator(video_list, 5)
     total_count = paginator.count
