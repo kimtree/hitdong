c0915ae12beffa7b8d2cde0f889e231ad6d7760b
Namwoo Kim <iam@kimtree.net>
2015-08-29 20:37:36 +0900

    Add video tagger

--- hitdong/apps/video/tasks.py
+++ hitdong/apps/video/tasks.py
@@ -43,42 +43,10 @@ def do_parse(type, origin_id):
 
 @task
 def video_tagger(video):
-    text_to_tag_id = {
-        'MV': 31,
-        u'여자친구': 5,
-        u'에이핑크': 6,
-        u'스텔라': 7,
-        u'헬로비너스': 8,
-        'HelloVenus': 8,
-        'Teaser': 32,
-        u'멜로디데이': 10,
-        'B1A4': 11,
-        u'달샤벳': 12,
-        u'미쓰에이': 13,
-        u'백아연': 14,
-        'AOA': 15,
-        u'소녀시대': 16,
-        u'아이유': 17,
-        u'피에스타': 18,
-        u'포미닛': 19,
-        u'걸스데이': 22,
-        u'레인보우': 23,
-        u'나인뮤지스': 24,
-        u'스피카': 25,
-        u'직캠': 30
-    }
-
-    tags = []
-    for k, v in text_to_tag_id.items():
-        if k.lower() in video.description.lower():
-            try:
-                tag = Tag.objects.get(pk=v)
-                tags.append(tag)
-            except:
-                pass
-
+    tags = Tag.objects.all()
     for tag in tags:
-        video.tags.add(tag)
+        if tag.name.lower() in (video.title.lower() or video.description.lower()):
+            video.tags.add(tag)
 
 
 @task
